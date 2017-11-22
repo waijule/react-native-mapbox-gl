b5e0c31862b5d2d3a34a294a05e415a5770aa8c0
Tennen <tennen@waijule.com>
2017-11-22 16:20:28 +0800

    [FIX] RN 0.50.1

--- android/src/main/java/com/mapbox/reactnativemapboxgl/ReactNativeMapboxGLPackage.java
+++ android/src/main/java/com/mapbox/reactnativemapboxgl/ReactNativeMapboxGLPackage.java
@@ -21,7 +21,6 @@ public class ReactNativeMapboxGLPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
